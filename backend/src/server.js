84ef2653d38c1777f47b38717739e6ac2bdb42eb
Eman <emanghaaly028@gmail.com>
2025-08-06 20:31:13 +0300

    change

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,14 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import rateLimiter from "./middleWare/rateLimiter.js";
 import cors from "cors";
 import path from "path";
+import { connectDB } from "./config/db.js";
+import notesRoutes from "./routes/notesRoutes.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
-const app=express();
-const PORT= process.env.PORT || 5001;
+const app = express();
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV !== "production") {
@@ -17,7 +17,9 @@ if (process.env.NODE_ENV !== "production") {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/notes", rateLimiter, notesRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
@@ -26,7 +28,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server started on port:", PORT);
-  });
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 });
