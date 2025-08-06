69312b488c1782e347485b5ea3ae9d77bfe465e9
Eman <emanghaaly028@gmail.com>
2025-08-06 18:20:03 +0300

    changed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,11 +45,13 @@ const Home= () => {
       {loading && <div className="text-center text-purple-400 py-10">Loading notes...</div>}
         {!loading && notes.length===0 && !isRateLimited && <NotesNotFound/>}
         {notes.length > 0 && !isRateLimited && (
+          <div className="max-h-screen overflow-y-auto p-4">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} setNotes={setNotes}   />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
+        </div> 
         )}
       </div>
     </div>
