22f8690232be6454edc321ee33daa3618b0eab61
Eman <emanghaaly028@gmail.com>
2025-08-06 19:57:37 +0300

    changes

--- backend/src/middleWare/rateLimiter.js
+++ backend/src/middleWare/rateLimiter.js
@@ -1,5 +1,5 @@
 import { Redis } from "@upstash/redis";
-import { Ratelimit, RedisTokenBucket } from "@upstash/ratelimit";
+import { Ratelimit } from "@upstash/ratelimit";
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
