db8b3e3138c97978b36dffc0ac23bd01314a58b6
Eman <emanghaaly028@gmail.com>
2025-08-06 17:33:54 +0300

    changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL= import.meta.env.MODE==="development" ?"http://localhost:5001" :"/notes";
+if(import.meta.env.MODE==="development" )
+BASE_URL="http://localhost:5001";
 
 const api= axios.create({
    baseURL:BASE_URL
