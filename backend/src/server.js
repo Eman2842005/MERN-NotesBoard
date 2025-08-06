5441607d8269653f511e1ce756305b7b1ab318e8
Eman <emanghaaly028@gmail.com>
2025-08-06 19:30:27 +0300

    change

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,24 +9,24 @@ import path from "path";
 dotenv.config();
 const app=express();
 const PORT= process.env.PORT || 5001;
-const __dirname =path.resolve();
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-    console.log("server started on port:",PORT)});
-});
-if(process.env.NODE_ENV!=="production"){
-app.use(cors({origin:"http://localhost:5173"}));
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
 }
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(rateLimiter);
-
-app.use("/notes",notesRoutes);
-
-if(process.env.NODE_ENV==="production"){
-app.use(express.static(path.join(__dirname,"../frontend/dist")));
-app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-});
+app.use("/notes", rateLimiter, notesRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on port:", PORT);
+  });
+});
