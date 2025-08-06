582dd89a35e254f09523a472d07d4920454c7584
Eman <emanghaaly028@gmail.com>
2025-08-06 19:41:03 +0300

    changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,11 +3,10 @@ let BASE_URL;
 if (import.meta.env.MODE === "development") {
   BASE_URL = "http://localhost:5001";
 } else {
-  BASE_URL = "https://notes-board-n65z.onrender.com";
+  BASE_URL = ""; 
 }
-
 const api = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true
+  baseURL: BASE_URL
 });
+
 export default api;
